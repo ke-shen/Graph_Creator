436117eede88187c1cc4e3e9d277447132e0a4ca
U-BSD\296697 <296697@SHS-10DCE3B28.beaverton.k12.or.us>
2017-06-01 13:13:28 -0700

    some progress

---
+++ main.cpp
@@ -0,0 +1,22 @@
+//Graph Creator
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <string.h>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+
+using namespace std;
+
+struct vertex {
+  char label;
+  vector<pair<vertex,int> *adjacency;
+}
+
+int main() {
+  vertex* init = new vertex();
+  return 0;
+}
+
+int add_vertex()
