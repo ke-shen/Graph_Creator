450de24650601802919537cab7a5b967d7e487bc
kevin shen <ikevinshen2@gmail.com>
2017-06-08 21:54:30 -0700

    progress ish

--- main.cpp
+++ main.cpp
@@ -10,13 +10,59 @@
 using namespace std;
 
 struct vertex {
-  char label;
-  vector<pair<vertex,int> *adjacency;
-}
+  char* label;
+  vector<pair<vertex*,int>* >  *adjacency;
+};
+
+void add_vertex(vector<vertex*>* list);
+void add_edge(vector<vertex*>* list);
+//checks if the vertex is located within the vector storing all vertices
+bool check_vertex(vector<vertex*>* list, char* label);
+
 
 int main() {
-  vertex* init = new vertex();
+  int i = 5;
+  vector<vertex*> * list = new vector<vertex*>;
+  add_vertex(list);
+  add_vertex(list);
+  add_vertex(list);
+  add_vertex(list);
+  add_vertex(list);
+  add_vertex(list);
+  add_vertex(list);
+
+
   return 0;
 }
 
-int add_vertex()
+void add_vertex(vector<vertex*>* list)
+{
+  char c[10];
+  cout << "Enter a label for the vertex (1 character): " << flush;
+  cin >> c;
+  if(!check_vertex(list, c)) {
+    vertex* n_vertex = new vertex;
+    //label is the first character of the input
+    strcpy(n_vertex->label, c);
+    list->push_back(n_vertex);
+    return;
+  }
+  else {
+    cout << "The label you entered is already in use in the graph. " << endl;
+    return;
+  }
+}
+
+void add_edge(vector<vertex*>* list)
+{
+  char temp[50];
+  cout << "Enter label of first vertex: " << flush;
+  cin >> temp;
+}
+
+bool check_vertex(vector<vertex*>* list, char* label)
+{
+    for(int i = 0; i < list->size(); i++)
+      if(!strcmp(list->at(i)->label, label)) return true;
+    return false;
+}
